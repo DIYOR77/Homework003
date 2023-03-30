b3822650565021790de2072bdb5e18dd91274171
unknown <129381997+diyor11111@users.noreply.github.com>
2023-03-30 15:35:55 +0500

    Second

--- homework003/homework003/Program.cs
+++ homework003/homework003/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write(" Son kiriting: ");
+            string son=Console.ReadLine();
+            bool isnumber = int.TryParse(son, out int raqam);
+            int faktorial = 1;
+            if (isnumber)
+            {
+                for (int i = 1; i <= raqam; i++)
+                {
+                    faktorial *= i;
+                }
+                Console.WriteLine($"{" Faktoriali: "}{faktorial}");
+            }
+            else
+            {
+                Console.Write(" Faqat son butun son kirita olasz !!!");
+            }
+            Main(args);
         }
     }
 }
